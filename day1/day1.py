2f5415527187351a3a0bfff9158b03d33570e01e
Ken Raffenetti <raffenet@mcs.anl.gov>
2024-12-02 09:14:17 -0600

    day1 solution

---
+++ day1/day1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#input = open('sample.txt', 'r')
+input = open('input.txt', 'r')
+left = [int(each.rstrip('\n').split()[0]) for each in input.readlines()]
+input.seek(0)
+right = [int(each.rstrip('\n').split()[1]) for each in input.readlines()]
+left.sort()
+right.sort()
+
+total = 0
+for i in range(len(left)):
+    total += abs(left[i] - right[i])
+print("part 1: " + str(total))
+
+similarity_score = 0
+for each in left:
+    similarity_score += each * right.count(each)
+print("part 2: " + str(similarity_score))
