a9352819753441654706d23066a0d0a86308bd0e
Ken Raffenetti <raffenet@mcs.anl.gov>
2024-12-06 12:33:05 -0600

    day 5: part 1 solution

--- day5/day5.py
+++ day5/day5.py
@@ -7,5 +7,21 @@ else:
 
 input = open(filename, 'r')
 foo = [each.rstrip() for each in input.readlines()]
-ordering_rules = [each.split('|') for each in foo[0:21]]
-updates = [each.split(',') for each in foo[22:]]
+ordering_rules = {}
+for each in foo[0:foo.index('')]:
+    k,v = tuple(map(int, each.split('|')))
+    if k in ordering_rules.keys():
+        ordering_rules[k].append(v)
+    else:
+        ordering_rules[k] = [v]
+updates = [map(int, each.split(',')) for each in foo[foo.index('')+1:]]
+
+def correct(update):
+    tmp = list(update)
+    for page in tmp:
+        if page in ordering_rules:
+            for rule in ordering_rules[page]:
+                if rule in tmp[:tmp.index(page)]:
+                    return 0
+    return tmp[int(len(tmp)/2)]
+print("part 1:", sum(map(correct, updates)))
