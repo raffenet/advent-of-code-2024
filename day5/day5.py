330277937b220c07e675b63f0e4c2a1ef0f4ff8f
Ken Raffenetti <raffenet@mcs.anl.gov>
2024-12-07 20:19:40 -0600

    day5: part 2 solution

--- day5/day5.py
+++ day5/day5.py
@@ -16,12 +16,28 @@ for each in foo[0:foo.index('')]:
         ordering_rules[k] = [v]
 updates = [map(int, each.split(',')) for each in foo[foo.index('')+1:]]
 
+incorrect = []
 def correct(update):
     tmp = list(update)
     for page in tmp:
         if page in ordering_rules:
             for rule in ordering_rules[page]:
                 if rule in tmp[:tmp.index(page)]:
+                    incorrect.append(tmp)
                     return 0
     return tmp[int(len(tmp)/2)]
 print("part 1:", sum(map(correct, updates)))
+
+def cmp(a, b):
+    if a in ordering_rules and b in ordering_rules[a]:
+        return -1
+    elif b in ordering_rules and a in ordering_rules[b]:
+        return 1
+    return 0
+
+import functools
+def fix(update):
+    update.sort(key=functools.cmp_to_key(cmp))
+    return update[int(len(update)/2)]
+
+print("part 2:", sum(map(fix, incorrect)))
